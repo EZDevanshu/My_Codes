9579f3ba2a61638441fae0c0ba20ae339ea359f0
Devanshu Parmar <devanshuparmar00@gmail.com>
2025-08-01 14:39:57 +0530

    LabCode

---
+++ LabCode_Java/Lab_1/Feet.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Feet 
+{
+    public static void main(String args[])
+
+    {
+        Scanner bc=new Scanner(System.in);
+
+        System.out.println("Enetr Feet");
+        int a= bc.nextInt();
+        System.out.println(a*12);
+
+    }
+}
