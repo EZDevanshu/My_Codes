b603ef289fd3bc16010ba591c64a5e5a8c5f4e38
Devanshu Parmar <devanshuparmar00@gmail.com>
2025-08-01 14:41:29 +0530

    LabCode

---
+++ LabCode_Java/Lab_1/Sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner ;
+public class Sum
+{
+   public static void main (String args[])
+   {
+     Scanner bs = new Scanner(System.in);
+     System.out.println("Enter a");
+     int a= bs.nextInt();
+     System.out.println("Enter b");
+     int b= bs.nextInt();
+     System.out.println(a+b);
+   }    
+}
