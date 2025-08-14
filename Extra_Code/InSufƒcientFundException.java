96130030ee4e598d61dc8db0421095dafb4315ea
Devanshu Parmar <devanshuparmar00@gmail.com>
2025-08-14 08:32:28 +0530

    Practice

---
+++ Extra_Code/InSufƒcientFundException.java
@@ -0,0 +1,9 @@
+
+public class InSufƒcientFundException {
+
+    public void printStackTrace() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printStackTrace'");
+    }
+
+}
