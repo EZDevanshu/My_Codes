417b5d397e3853012f5a236ba0ce90c08a1875d8
Devanshu Parmar <devanshuparmar00@gmail.com>
2025-08-18 15:04:53 +0530

    Practice

---
+++ Extra_Code/CustomException.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class InSuffcientFundException extends Exception{
+     
+}
+public class CustomException {
+    int curentBlance = 5000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Amout : ");
+        int withdraw = sc.nextInt();
+
+    }
+}
