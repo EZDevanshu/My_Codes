eb210f51aa72ea81fb0754ea5800eb103a1c5720
Devanshu Parmar <devanshuparmar00@gmail.com>
2025-09-15 23:00:45 +0530

    LeetCode

--- LeetCode_Practice/Leet_442.java
+++ LeetCode_Practice/Leet_442.java
@@ -10,7 +10,7 @@ class Solution {
         while(i < arr.length){
             int correct = arr[i] - 1;
             if(arr[i] <= arr.length && arr[i] != arr[correct]){
-                swap(arr , i , correct);
+                swap(arr, i, correct);
             }
             else{
                 i++;
