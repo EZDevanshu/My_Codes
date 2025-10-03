3ed2626b17021c84c94ec86e3277bebb4854701b
Devanshu Parmar <devanshuparmar00@gmail.com>
2025-10-03 10:45:57 +0530

    Leet_1523.java

---
+++ LeetCode_Practice/Leet_1523.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        int count=0;
+    if(low%2==1&&high%2==1) count=((high-low)/2)+1;
+    if(low%2==0&&high%2==0)count=((high-low)/2);
+    if(low%2==0&&high%2==1)count=((high-low)/2)+1;
+    if(low%2==1&&high%2==0)count=((high-low)/2)+1;
+     return count;  
+    }
+}
