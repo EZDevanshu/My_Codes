fdfe67ed882dac4f984dcdbb7f086acca94c786c
Devanshu Parmar <devanshuparmar00@gmail.com>
2025-12-21 12:08:22 +0530

    Delete MyCodes/LeetCode_Practice directory

--- MyCodes/LeetCode_Practice/Leet_1464.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int maxProduct(int[] nums) {
-        
-        int n = nums.length;
-    	Arrays.sort(nums);
-    	
-    	return (nums[n - 1 ] - 1) * (nums[n - 2] - 1);
-    }
-}
